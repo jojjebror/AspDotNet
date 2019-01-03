57bac11f0810cb24eedd78e8c62069874a189bea
jojjebror <jjohaan@live.se>
2019-01-03 17:14:51 +0100

    johan

---
+++ Taharifran/Models/UserDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Taharifran.Models
+{
+    public class UserDbContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UserDbContext() : base("userdb") { }
+    }
+}
---
+++ Taharifran/Models/UserIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Taharifran.Models
+{
+    public class UserIndexViewModel
+    {
+        public List<User> Users { get; set; }
+
+
+    }
+}
